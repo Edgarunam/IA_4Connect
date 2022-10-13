ca449d958952aae960147be04301ea823edd6f18
Edgarunam <etobon96@gmail.com>
2022-10-13 12:21:50 -0500

    First Smart IA [ROW]

--- connect4.py
+++ connect4.py
@@ -18,6 +18,10 @@ RED = (255,0,0)
 
 PLAYER:int =0
 AI:int = 1
+EMPTY:int = 0
+PLAYER_PIECE:int = 1
+AI_PIECE:int = 2
+WINDOW_LENGTH:int = 4
 #------------------------------------------------------------------------------------------------------------------------------------------
 
 #MARK: FUNCITIONS
@@ -67,12 +71,84 @@ def winning_move(board,piece):
             if board[r][c]==piece and board[r+1][c+1]==piece and board[r+2][c+2] == piece and board [r+3][c+3]== piece:
                 return True
 
+    #Diagonal Check (-) #Horizontal checl
+    for c in range (COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1]==piece and board[r][c+2]== piece and board[r][c+3]== piece:
+                return True
+
+    #Vertical Check
+
+    for r in range (ROW_COUNT-3):
+        for c in range (COLUMN_COUNT):
+            if board[r][c]==piece and board[r+1][c]==piece and board[r+2][c] == piece and board [r+3][c]== piece:
+                return True
+
+    #Diagonal Check (+)
+
+    for r in range (ROW_COUNT-3):
+        for c in range (COLUMN_COUNT-3):
+            if board[r][c]==piece and board[r+1][c+1]==piece and board[r+2][c+2] == piece and board [r+3][c+3]== piece:
+                return True
+
     #Diagonal Check (-)
 
     for r in range (3,ROW_COUNT):
         for c in range (3,COLUMN_COUNT-3):
             if board[r][c]==piece and board[r-1][c+1]==piece and board[r-2][c+2] == piece and board [r-3][c+3]== piece:
                 return True
+
+    for r in range (3,ROW_COUNT):
+        for c in range (3,COLUMN_COUNT-3):
+            if board[r][c]==piece and board[r-1][c+1]==piece and board[r-2][c+2] == piece and board [r-3][c+3]== piece:
+                return True
+
+
+###HEURISTC FUNCTION###
+
+def score_position(board,piece):
+    score = 0
+    #Horizontal Score
+    for r in range(ROW_COUNT):
+        row_array = [int(i) for i in list(board[r,:])]
+        for c in range(COLUMN_COUNT-3):
+            window = row_array[c:c+WINDOW_LENGTH]
+            if window.count(piece) == 4:
+                score += 100
+            elif window.count(piece) == 3 and window.count(EMPTY)==1:
+                score += 10
+                                           
+            
+    return score
+def get_valid_locations(board):
+    valid_locations = []
+    for c in range(COLUMN_COUNT-1):
+        if is_valid_location(board,c):
+            valid_locations.append(c)
+    return valid_locations
+
+
+def pick_best_move(board,piece):
+    valid_locations = get_valid_locations(board)
+    best_score = 0
+    best_col = random.choice(valid_locations)
+    
+    #ITERATION FOR LOOKING THE BEST COL 
+    for col in valid_locations:
+        row = get_next_open_row(board,col)
+        temp_board = board.copy()
+        drop_piece(temp_board,row,col,piece)
+        score = score_position(temp_board,piece)
+        if score > best_score:
+            best_score = score
+            best_col = col
+    return best_col
+
+
+        
+
+
+
 
 pygame.init()
 myfont = pygame.font.SysFont("monospace",75)
@@ -107,7 +183,7 @@ def run():
     width = COLUMN_COUNT * SQUARESIZE
     board = create_board()
     game_over = False
-    turn = 0
+    turn = random.randint(PLAYER,AI)
     draw_board(board)
     pygame.display.update()
 
@@ -142,10 +218,10 @@ def run():
                     col = int(math.floor(posX/SQUARESIZE))
                     if is_valid_location(board,col):
                         row = get_next_open_row(board,col)
-                        drop_piece(board,row,col,1)
+                        drop_piece(board,row,col,PLAYER_PIECE)
                         
 
-                        if(winning_move(board,1)):
+                        if(winning_move(board,PLAYER_PIECE)):
                             pygame.draw.rect(screen,BLACK,(0,0, width,SQUARESIZE))
                             label = myfont.render("YOU WIN",1,RED)
                             screen.blit(label,(40,10))
@@ -165,14 +241,15 @@ def run():
         if turn ==AI and not game_over:
 
                     
-            col = random.randint(0,COLUMN_COUNT-1)
+            #col = random.randint(0,COLUMN_COUNT-1)
+            col = pick_best_move(board,AI_PIECE)
             time.sleep(0.2)
 
             if is_valid_location(board,col):
                 row = get_next_open_row(board,col)
-                drop_piece(board,row,col,2)
+                drop_piece(board,row,col,AI_PIECE)
                         
-                if(winning_move(board,2)):
+                if(winning_move(board,AI_PIECE)):
                     pygame.draw.rect(screen,BLACK,(0,0, width,SQUARESIZE))
                     label = myfont.render("IA WON",1,YELLOW)
                     screen.blit(label,(40,10))
